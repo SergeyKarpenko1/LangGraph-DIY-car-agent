583c40f46c399e1914b274f3b986be6e50249c8d
SergeyKarpenko1 <skarp35@gmail.com>
2025-12-12 21:00:24 +0300

    Добавил константы из агента

--- lib/utils/constant.py
+++ lib/utils/constant.py
@@ -1,4 +1,10 @@
 import os
 
-USE_RERANKER = os.getenv("USE_RERANKER", "1").lower() in ("1", "true", "yes")
+# retrieval
+USE_RERANKER = os.getenv("USE_RERANKER", "0").lower() in ("1", "true", "yes")
 RERANK_TOP_K = int(os.getenv("RERANK_TOP_K", "3"))
+
+# multystep_reasoning_agent
+DOCS_KEY = "documents"
+GO_FLAG  = "proceed_to_generate"
+MAX_REPHRASES = 1
